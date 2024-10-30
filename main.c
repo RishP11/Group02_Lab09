2a59dc7791a0798c84d23266b0c765af7411d0f5
Rishabh Pomaje <97334879+RishP11@users.noreply.github.com>
2024-10-30 20:45:58 +0530

    code clean-up

--- main.c
+++ main.c
@@ -7,7 +7,6 @@
 * Using I2C module 3 <--> interfaced through Port D
 */
 #define CLOCK_HZ    16000000
-#define DAC_ADDR    0x60
 #define WAVERES     500
 #define PI          3.14159265358979
 #define maxRES      4096
@@ -30,32 +29,31 @@ uint16_t sawTable[maxRES] ;
 
 int main(void)
 {
-    CLK_enable();                                               // Enable all the required Clocks
+    CLK_enable();
     PORT_D_init();
     I2C3_setup();
     while(1) {
-//        sineWave();
-        sawToothWave( 2.5 ) ;
+        // By default start with sine wave:
+        sineWave() ;
     }
 }
 
 void CLK_enable( void )
 {
-    // Setting up all the clocks
-    SYSCTL_RCGCI2C_R |= 0x8 ;                             // Enabling the clock to UART module 7
-    SYSCTL_RCGCGPIO_R |= 0x00008 ;
+    // Setting up all the clocks to
+    SYSCTL_RCGCI2C_R |= 0x8 ;                               // I2C module 3
+    SYSCTL_RCGCGPIO_R |= 0x00008 ;                          // GPIO Port D
 }
 
 void PORT_D_init( void )
 {
-    // Configure this port as a I2C master
-    GPIO_PORTD_LOCK_R = 0x4C4F434B ;                            // Unlock commit register
-    GPIO_PORTD_CR_R |= 0xFF ;                                    // Make PORT-E configurable
-    GPIO_PORTD_DEN_R  = 0x03 ;                                   // Set PORT-E pins as digital pins
-    GPIO_PORTD_AFSEL_R = 0x03 ;
-    GPIO_PORTD_PCTL_R = 0x33 ;                                  // Selecting the peripheral for the driving AFSEL
-    GPIO_PORTD_ODR_R = 0x02 ;
-    GPIO_PORTD_PUR_R = 0x03 ;
+    GPIO_PORTD_LOCK_R = 0x4C4F434B ;                        // Unlock commit register
+    GPIO_PORTD_CR_R |= 0xFF ;                               // Make PORT-D configurable
+    GPIO_PORTD_DEN_R  = 0x03 ;                              // Set PORT-D pins as digital pins
+    GPIO_PORTD_AFSEL_R = 0x03 ;                             // Selecting the alternate function
+    GPIO_PORTD_PCTL_R = 0x33 ;                              // Selecting the peripheral for the driving AFSEL
+    GPIO_PORTD_ODR_R = 0x02 ;                               // Open Drain Register
+    GPIO_PORTD_PUR_R = 0x03 ;                               // Pull-Up Register
 }
 
 void I2C3_setup( void )
@@ -66,9 +64,9 @@ void I2C3_setup( void )
     int SCL_CLK = 100000 ;
     int TPR = (1.0 * CLOCK_HZ / (2 * (SCL_HP + SCL_LP) * SCL_CLK)) - 1 ;
 
-    I2C3_MCR_R = 0x10 ;                                         // Master Control Register
-    I2C3_MTPR_R = TPR ;
-    I2C3_MSA_R = 0xC0 ;
+    I2C3_MCR_R = 0x10 ;                                     // |GFE|SFE|MFE|.|.|.|LPBK|
+    I2C3_MTPR_R = TPR ;                                     // Time Period Register
+    I2C3_MSA_R = 0xC0 ;                                     // |.|.|.|.|.|.|.|.|R/W'|
 }
 
 void sineWave( void )
@@ -76,30 +74,29 @@ void sineWave( void )
     int sample_idx ;
     uint16_t hexVal ;
 
+    // Pre-computing the sine values.
     for (sample_idx = 0; sample_idx < WAVERES; sample_idx++){
         sineTable[sample_idx] = 2048 + 2048  * (sin(2 * PI * 10 * sample_idx / WAVERES)) ;
     }
 
     while(1){
         // If switch 02 is pressed then exit and go to the other function
+        if (GPIO_PORTF_DATA_R & (0x01)){
+            sawToothWave(3.3) ;
+        }
         // else continue with the same waveform
         for (sample_idx = 0; sample_idx < WAVERES; sample_idx++){
-//            hexVal = 2048 + 2048  * (sin(2 * PI * 10 * sample_idx / WAVERES)) ; // Value to send
             hexVal = sineTable[sample_idx] ;
-            I2C3_MDR_R = (hexVal >> 8) ;              // Send the first byte
-            I2C3_MCS_R = 0x03;              // Start + Run
+            I2C3_MDR_R = (hexVal >> 8) ;                    // Send the first byte
+            I2C3_MCS_R = 0x03;                              // |HS|ACK|STOP|START|RUN|
 
-            while(I2C3_MCS_R & 0x01);       // Wait until transmission completes
-//            if (I2C3_MCS_R & 0x02) {        // Check for errors
-//            I2C3_MCS_R = 0x04;          // Send Stop if error
-//            return;
-//            }
-            I2C3_MDR_R = (hexVal & 0xFF) ;              // Send the next byte
-            I2C3_MCS_R = 0x05;              // Run + Stop
+            while(I2C3_MCS_R & 0x01);                       // |CLKTO|BUSBSY|IDLE|ARBLST|DATACK|ADRACK|ERROR|BUSY|
+            I2C3_MDR_R = (hexVal & 0xFF) ;                  // Send the next byte
+            I2C3_MCS_R = 0x05;                              // |HS|ACK|STOP|START|RUN|
 
-            while(I2C3_MCS_R & 0x01);       // Wait until transmission completes
-            if (I2C3_MCS_R & 0x02) {        // Check for errors
-            I2C3_MCS_R = 0x04;          // Send Stop if error
+            while(I2C3_MCS_R & 0x01);                       // |CLKTO|BUSBSY|IDLE|ARBLST|DATACK|ADRACK|ERROR|BUSY|
+            if (I2C3_MCS_R & 0x02) {
+                I2C3_MCS_R = 0x04;
             }
         }
     }
@@ -107,33 +104,33 @@ void sineWave( void )
 
 void sawToothWave( float Vmax )
 {
-
     int num_samples = (1.0 * maxRES * (Vmax / Vref)) ;
-
     uint16_t hexVal = 0 ;
     int i ;
     while(1){
-        // If switch 02 is pressed then exit and go to the other function
+        // If switch 01 is pressed then exit and go to the other function
+        if (GPIO_PORTF_DATA_R & (0x10)){
+             sineWave() ;
+         }
         // else continue with the same waveform
         for (i = 0; i < num_samples; i++){
-//            hexVal = 2048 + 2048  * (sin(2 * PI * 10 * sample_idx / WAVERES)) ; // Value to send
             hexVal += 0x001 ;
-            I2C3_MDR_R = (hexVal >> 8) ;              // Send the first byte
-            I2C3_MCS_R = 0x03;              // Start + Run
+            I2C3_MDR_R = (hexVal >> 8) ;                    // Send the first byte
+            I2C3_MCS_R = 0x03;                              // |HS|ACK|STOP|START|RUN|
 
-            while(I2C3_MCS_R & 0x01);       // Wait until transmission completes
-            if (I2C3_MCS_R & 0x02) {        // Check for errors
-            I2C3_MCS_R = 0x04;          // Send Stop if error
-            return;
+            while(I2C3_MCS_R & 0x01);                       // |CLKTO|BUSBSY|IDLE|ARBLST|DATACK|ADRACK|ERROR|BUSY|
+            if (I2C3_MCS_R & 0x02) {
+                I2C3_MCS_R = 0x04;                          // |HS|ACK|STOP|START|RUN|
+                return;
             }
-            I2C3_MDR_R = (hexVal & 0xFF) ;              // Send the next byte
-            I2C3_MCS_R = 0x05;              // Run + Stop
+            I2C3_MDR_R = (hexVal & 0xFF) ;                  // Send the next byte
+            I2C3_MCS_R = 0x05;                              // |HS|ACK|STOP|START|RUN|
 
-            while(I2C3_MCS_R & 0x01);       // Wait until transmission completes
-            if (I2C3_MCS_R & 0x02) {        // Check for errors
-            I2C3_MCS_R = 0x04;          // Send Stop if error
+            while(I2C3_MCS_R & 0x01);                       // |CLKTO|BUSBSY|IDLE|ARBLST|DATACK|ADRACK|ERROR|BUSY|
+            if (I2C3_MCS_R & 0x02) {
+                I2C3_MCS_R = 0x04;                          // |HS|ACK|STOP|START|RUN|
             }
         }
-        hexVal = 0 ;
+        hexVal = 0 ;                                        // Reset the triangular wave.
     }
 }
